560628e7d3337ef18087076027bf2a41e3d436f7
Andrés Javier López <ajavier.lopez@gmail.com>
2015-05-25 09:34:54 -0600

    Adds played matches to the view

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,6 +6,7 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE tournament;
 -- Creating the database
 CREATE DATABASE tournament;
 \c tournament
@@ -32,9 +33,18 @@ CREATE TABLE matches (
 );
 
 -- View with the player standings
-
 CREATE VIEW standings AS
-SELECT players.id, players.name, count(matches.id_winner) as won_matches
-FROM players LEFT JOIN matches ON players.id = matches.id_winner
-GROUP BY players.id
-ORDER BY won_matches;
+SELECT players.id, players.name, won.won_matches, played.played_matches
+FROM players
+JOIN (SELECT players.id, count(matches.id_winner) as won_matches FROM players
+LEFT JOIN matches ON players.id = matches.id_winner GROUP BY players.id) as won
+ON won.id = players.id
+JOIN (SELECT players.id, count(*) as played_matches FROM matches, players 
+WHERE id_player1 = players.id OR id_player2 = players.id GROUP BY players.id) 
+AS played ON players.id = played.id 
+ORDER BY won_matches DESC;
+
+-- Testing the database
+-- INSERT INTO players (name) VALUES ('uno'), ('dos'), ('tres'), ('cuatro');
+-- INSERT INTO matches VALUES (1,2,1), (3,4,3), (1,3,1), (2,4,2);
+-- SELECT * FROM standings;
